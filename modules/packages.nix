fc87950e522726be681b3e7051e4f8dd634fc68f
Don Williams <don.e.williams@gmail.com>
2025-09-21 02:41:00 -0400

    Added ddubos version

--- modules/packages.nix
+++ modules/packages.nix
@@ -68,27 +68,26 @@
     '')
 
     # Wfetch Randomizer script
-        (pkgs.writeShellScriptBin "wf" ''
-        # Wfetch Randomizer
-        # Choose between multiple command options randomly
-        # Author: Don Williams
-        # Revision History
-        #==============================================================
-        v0.1      5-15-2025        Initial release
+    (pkgs.writeShellScriptBin "wf" ''
+      # Wfetch Randomizer
+      # Choose between multiple command options randomly
+      # Author: Don Williams
+      # Revision History
+      #==============================================================
+      v0.1      5-15-2025        Initial release
 
-        # Generate a random number (0 to 4)
-        choice=$((RANDOM % 5))
+      # Generate a random number (0 to 4)
+      choice=$((RANDOM % 5))
 
-        # Execute one of the five commands based on the random number
-        case "$choice" in
-            0) wfetch --waifu2 --challenge --challenge-years=3 --image-size 300 ;;
-            1) wfetch --waifu --challenge --challenge-years=3 --image-size 300 ;;
-            2) wfetch --challenge --challenge-years=3 --hollow ;;
-            3) wfetch --challenge --challenge-years=3 --wallpaper ;;
-            4) wfetch --challenge --challenge-years=3 --smooth ;;
-        esac
-        '')
-
+      # Execute one of the five commands based on the random number
+      case "$choice" in
+          0) wfetch --waifu2 --challenge --challenge-years=3 --image-size 300 ;;
+          1) wfetch --waifu --challenge --challenge-years=3 --image-size 300 ;;
+          2) wfetch --challenge --challenge-years=3 --hollow ;;
+          3) wfetch --challenge --challenge-years=3 --wallpaper ;;
+          4) wfetch --challenge --challenge-years=3 --smooth ;;
+      esac
+    '')
 
     # Hyprland Stuff
     hypridle
@@ -174,7 +173,7 @@
     qt6ct
     qt6.qtwayland
     qt6Packages.qtstyleplugin-kvantum # kvantum
-     gsettings-qt
+    gsettings-qt
     rofi
     slurp
     swappy
@@ -253,15 +252,19 @@
 
     # Video
     vlc
-    #jellyfin-media-player   #Causes failed builds 
+    #jellyfin-media-player   #Causes failed builds
 
     # Terminals
     kitty
     wezterm
     ptyxis
-    remmina 
-
+    remmina
 
   ];
 
+  environment.variables = {
+    NIXOS_OZONE_WL = "1";
+    DDUBSOS_VERSION = "NixOS-0.4";
+    DDUBSOS = "true";
+  };
 }
