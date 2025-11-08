6837a8cbb5b8533d07cc5344bad5a76369644db2
Don Williams <don.e.williams@gmail.com>
2025-11-07 23:12:36 -0500

    Fixed eval warnings
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
        modified:   modules/home/cli/git.nix

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -1,38 +1,10 @@
 { lib, ... }:
 {
+  # 1. Main Git Configuration
   programs.git = {
     enable = true;
 
-    # If you want to set identity per-host, you can do it in a small overlay
-    # or via host variables. Leaving these unset keeps git functional and avoids
-    # a build-time dependency on external variables.
-    # userName = "Your Name";
-    # userEmail = "you@example.com";
-
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        side-by-side = true;
-        true-color = "never";
-
-        # Combine Catppuccin delta theme with our local feature set
-        features = lib.mkForce "catppuccin-mocha unobtrusive-line-numbers decorations";
-        unobtrusive-line-numbers = {
-          line-numbers = true;
-          line-numbers-left-format = "{nm:>4}│";
-          line-numbers-right-format = "{np:>4}│";
-          line-numbers-left-style = "grey";
-          line-numbers-right-style = "grey";
-        };
-        decorations = {
-          commit-decoration-style = "bold grey box ul";
-          file-style = "bold blue";
-          file-decoration-style = "ul";
-          hunk-header-decoration-style = "box";
-        };
-      };
-    };
+    # userEmail and userName comments remain...
 
     settings = {
       push.default = "simple"; # Match modern push behavior
@@ -46,20 +18,47 @@
       diff.colorMoved = "default";
       merge.stat = "true";
       core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
+      alias = {
+        br = "branch --sort=-committerdate";
+        co = "checkout";
+        af = "!git add $(git ls-files -m -o --exclude-standard | fzf -m)";
+        com = "commit -a";
+        ca = "commit -a";
+        df = "diff";
+        gs = "stash";
+        gp = "pull";
+        st = "status";
+        lg = "log --graph --pretty=format:'%Cred%h%Creset - %C(yellow)%d%Creset %s %C(green)(%cr)%C(bold blue) <%an>%Creset' --abbrev-commit";
+        edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      };
     };
-    # Optional: Git aliases
-    aliases = {
-      br = "branch --sort=-committerdate";
-      co = "checkout";
-      af = "!git add $(git ls-files -m -o --exclude-standard | fzf -m)";
-      com = "commit -a";
-      ca = "commit -a";
-      df = "diff";
-      gs = "stash";
-      gp = "pull";
-      st = "status";
-      lg = "log --graph --pretty=format:'%Cred%h%Creset - %C(yellow)%d%Creset %s %C(green)(%cr)%C(bold blue) <%an>%Creset' --abbrev-commit";
-      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+    # The old `aliases` block is now inside `settings = { alias = { ... }; };`
+  };
+
+  # 2. Delta Configuration (moved from `programs.git.delta`)
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      side-by-side = true;
+      true-color = "never";
+
+      # Combine Catppuccin delta theme with our local feature set
+      features = lib.mkForce "catppuccin-mocha unobtrusive-line-numbers decorations";
+      unobtrusive-line-numbers = {
+        line-numbers = true;
+        line-numbers-left-format = "{nm:>4}│";
+        line-numbers-right-format = "{np:>4}│";
+        line-numbers-left-style = "grey";
+        line-numbers-right-style = "grey";
+      };
+      decorations = {
+        commit-decoration-style = "bold grey box ul";
+        file-style = "bold blue";
+        file-decoration-style = "ul";
+        hunk-header-decoration-style = "box";
+      };
     };
   };
 }
